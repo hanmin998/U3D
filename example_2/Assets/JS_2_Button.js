de59d821183069ad47917d68a825bedc73088861
hanmin998 <853049349@qq.com>
2014-06-01 11:31:47 +0800

    Controls
    examples for those controls

---
+++ example_2/Assets/JS_2_Button.js
@@ -0,0 +1,40 @@
+﻿#pragma strict
+
+//按钮贴图
+var buttonTexture:Texture2D;
+//提示信息
+private var str:String;
+//时间计数器
+private var frameTime:int;
+
+function Start () {
+	//初始化赋值
+	str="请你点击按钮";
+}
+
+function OnGUI () {
+	//显示提示信息内容
+	GUI.Label(Rect(300,10,Screen.width,30),str);
+	if(GUI.Button(Rect(300,140,buttonTexture.width,buttonTexture.height),buttonTexture)){
+		//点击按钮修改提示信息
+		str="您点击了图片按钮";
+	}
+	//设置按钮中文字颜色
+	GUI.color=Color.green;
+	//设置按钮的背景色
+	GUI.backgroundColor=Color.red;
+	if(GUI.Button(Rect(300,50,70,30),"文字按钮")){
+		//点击按钮修改提示信息
+		str="您点击了文字按钮";
+	}
+	//设置按钮中文字颜色
+	GUI.color=Color.yellow;
+	//设置按钮的背景色
+	GUI.backgroundColor=Color.blue;
+	if(GUI.RepeatButton(Rect(300,100,100,30),"按钮按下中")){
+		//点击按钮修改提示信息
+		str="按钮按下中的时间："+frameTime;
+		//时间计数器
+		frameTime++;
+	}
+}
---
+++ example_2/Assets/JS_2_Label.js
@@ -0,0 +1,32 @@
+﻿#pragma strict
+
+
+//接收外部赋值字符串
+var str:String;
+//接收外部赋值贴图
+var imageTexture:Texture;
+//贴图宽度
+private var imageWidth:int;
+//贴图高度
+private var imageHeight:int;
+//当前屏幕宽度
+private var screenWidth:int;
+//当前屏幕高度
+private var screenHeight:int;
+function Start () {
+	//得到屏幕宽高
+	screenWidth=Screen.width;
+	screenHeight=Screen.height;
+	//得到图片宽高
+	imageWidth=imageTexture.width;
+	imageHeight=imageTexture.height;
+}
+
+function OnGUI () {
+	//将文字内容显示在屏幕中
+	GUI.Label(Rect(100,10,100,30),str);
+	GUI.Label(Rect(100,40,100,30),"当前屏幕宽："+screenWidth);
+	GUI.Label(Rect(100,80,100,30),"当前屏幕高："+screenHeight);
+	//将贴图显示在屏幕中
+	GUI.Label(Rect(100,120,imageWidth,imageHeight),imageTexture);
+}
