fbe8241f0ffb22380373914edfeff7e121a7e745
hanmin998 <853049349@qq.com>
2014-06-04 19:17:21 +0800

    CustomGUI GUILayoutOption GUILayout
    CustomGUI GUILayoutOption GUILayout

---
+++ example_2/Assets/JS_2_CustomGUI.js
@@ -0,0 +1,12 @@
+﻿#pragma strict
+
+//自定义皮肤
+var mySkin:GUISkin;
+
+function OnGUI () {
+	//设置GUI皮肤为我们自定义的皮肤
+	GUI.skin=mySkin;
+	//绘制按钮，通过名称找到对应风格
+	GUI.Button(Rect(0,100,300,100),"","Custom0");
+	GUI.Button(Rect(300,100,300,100),"","Custom1");
+}
---
+++ example_2/Assets/JS_2_GUILayout.js
@@ -0,0 +1,12 @@
+﻿#pragma strict
+var addStr:String="待测试字符串";
+function OnGUI () {
+	//普通按钮
+	if(GUI.Button(Rect(50,50,100,30),addStr)){
+		addStr+=addStr;
+	}
+	//界面布局按钮
+	if(GUILayout.Button(addStr)){
+		addStr+=addStr;
+	}
+}
