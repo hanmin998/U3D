fc4e9e507b079e1271406948bec89e88dcd97b61
hanmin998 <853049349@qq.com>
2014-06-06 08:57:08 +0800

    animation
    animation

---
+++ example_3/Assets/Resources/JS_3_Texture.js
@@ -0,0 +1,34 @@
+﻿#pragma strict
+//贴图
+private var texSingle:Texture2D;
+//贴图数组
+private var texAll:Object[];
+
+function OnGUI () {
+	if(GUI.Button(Rect(0,10,100,50),"加载第一张贴图")){
+		if(texSingle==null){
+			//加载贴图
+			texSingle=Resources.Load("Single/1");
+		}
+	}
+	if(GUI.Button(Rect(0,130,100,50),"加载一组贴图")){
+		if(texAll==null){
+			//加载所有贴图
+			texAll=Resources.LoadAll("texAll");
+		}
+	}
+	//绘制贴图
+	if(texSingle!=null){
+		//绘制一张贴图
+		GUI.DrawTexture(Rect(110,10,120,120),texSingle,ScaleMode.StretchToFill
+		,true,0);
+	}
+	if(texAll!=null){
+		for(var i=0;i<texAll.length;i++){
+			//绘制贴图
+			GUI.DrawTexture(Rect(110+i*120,130,120,120),texAll[i],
+			ScaleMode.StretchToFill,true,0);
+		}
+	}
+}
+
---
+++ example_3/Assets/Resources/Js_3_animation.js
@@ -0,0 +1,41 @@
+﻿#pragma strict
+//动画数组
+private var anim:Object[];
+//帧序列
+private var nowFram:int;
+//动画帧的总数
+private var mFrameCount:int;
+//限制一秒多少帧
+private var fps:float=5;
+//限制帧的时间
+private var time:float=0;
+function Start () {
+	//得到帧动画中的所有图片资源
+	anim=Resources.LoadAll("animation");
+	//得到该动画共有多少帧
+	mFrameCount=anim.Length;
+}
+
+function OnGUI () {
+	//绘制动画
+	DrawAnimation(anim,Rect(100,100,32,48));
+}
+function DrawAnimation(tex:Object[],rect:Rect){
+	//绘制动画信息
+	GUILayout.Label("当前动画播放：第"+nowFram+"帧");
+	//绘制当前帧
+	GUI.DrawTexture(rect,tex[nowFram],ScaleMode.StretchToFill,true,0);
+	//计算限制帧时间
+	time+=Time.deltaTime;
+	//超过限制帧则切换图片
+	if(time>=1.0/fps){
+		//帧序列切换
+		nowFram++;
+		//限制帧清空
+		time=0;
+		//超过帧动画总是，从0开始
+		if(nowFram>=mFrameCount){
+			nowFram=0;
+		}
+	}
+}
