e77f8fb535c98ee830e9cfda70c493534d31f09c
hanmin998 <853049349@qq.com>
2014-06-07 21:06:21 +0800

    2D Toolkit v2.3.3
    2D Toolkit v2.3.3

---
+++ example_3/Assets/Resources/JS_3_2DGame.js
@@ -0,0 +1,90 @@
+﻿#pragma strict
+
+//主角对象
+private var hero:GameObject;
+//按键是否被按下
+private var keyUp:boolean;
+private var keyDown:boolean;
+private var keyLeft:boolean;
+private var keyRight:boolean;
+//记录当前时间
+private var time:float;
+//限制一秒多少帧
+private var fps:float=10;
+//帧序列
+private var nowFram:int;
+//动画数组
+private var animUp:Object[];
+private var animDown:Object[];
+private var animLeft:Object[];
+private var animRight:Object[];
+private var nowAnim:Object[];
+private var backAnim:Object[];
+function Start () {
+	//得到主角对象
+	hero=GameObject.Find("hero");
+	//得到上下左右四组动画
+	animUp=Resources.LoadAll("up");
+	animDown=Resources.LoadAll("down");
+	animLeft=Resources.LoadAll("left");
+	animRight=Resources.LoadAll("right");
+	nowAnim=animDown;
+	backAnim=animDown;
+}
+
+function OnGUI() {
+	//控制主角移动的按钮
+	keyUp=GUILayout.RepeatButton("向上");
+	keyDown=GUILayout.RepeatButton("向下");
+	keyLeft=GUILayout.RepeatButton("向左");
+	keyRight=GUILayout.RepeatButton("向右");
+}
+function FixedUpdate(){
+	if(keyUp){
+		//向上移动
+		SetAnimation(animUp);
+		hero.transform.Translate(-Vector3.forward*0.001f);
+	}
+	if(keyDown){
+		//向下移动
+		SetAnimation(animDown);
+		hero.transform.Translate(Vector3.forward*0.001f);
+	}
+	if(keyLeft){
+		//向左移动
+		SetAnimation(animLeft);
+		hero.transform.Translate(Vector3.right*0.001f);
+	}
+	if(keyRight){
+		//向右移动
+		SetAnimation(animRight);
+		hero.transform.Translate(-Vector3.right*0.001f);
+	}
+	//播放动画
+	DrawAnimation(nowAnim);
+}
+function DrawAnimation(tex:Object[]){
+	//计算限制帧的时间
+	time+=Time.deltaTime;
+	//超过限制帧则切换贴图
+	if(time>1.0/fps){
+		//帧序列切换
+		nowFram++;
+		//限制帧清空
+		time=0;
+		//超过帧动画总数时，从0开始
+		if(nowFram>=tex.Length){
+			nowFram=0;
+		}
+	}
+	//将对应的贴图赋予主角对象
+	hero.renderer.material.mainTexture=tex[nowFram];
+}
+function SetAnimation(tex:Object[]){
+	//设置播放动画
+	nowAnim=tex;
+	if(!backAnim.Equals(nowAnim)){
+		nowFram=0;
+		backAnim=nowAnim;
+	}
+}
---
+++ example_3/Assets/Resources/JS_3_Tool.js
@@ -0,0 +1,68 @@
+﻿#pragma strict
+
+//背景贴图
+private var bg:Texture2D;
+//标题贴图
+private var title:Texture2D;
+//动画数组
+private var tex:Object[];
+//动画X坐标
+private var x:int;
+//动画Y坐标
+private var y:int;
+//帧序列
+private var nowFram:int;
+//动画帧的总数
+private var mFrameCount:int;
+//限制一秒多少帧
+private var fps:float=5;
+//限制帧时间
+private var time:float=0;
+
+function Start () {
+	//载入资源
+	bg=Resources.Load("bg");
+	title=Resources.Load("title");
+	tex=Resources.LoadAll("anim");
+	//初始化动画坐标
+	x=Screen.width;
+	y=200;
+}
+
+function OnGUI () {
+	//绘制贴图
+	GUI.DrawTexture(Rect(0,0,Screen.width,Screen.height),bg,
+	ScaleMode.StretchToFill,true,0);
+	//绘制标题
+	GUI.DrawTexture(Rect((Screen.width-title.width)>>1,30,title.width,title.height),
+	title,ScaleMode.StretchToFill,true,0);
+	//绘制帧动画
+	DrawAnimation(tex,Rect(x,y,40,60));
+	//动画越界检测
+	x--;
+	if(x<-42){
+		x=480;
+	}
+	//绘制按钮
+	GUI.Button(Rect(230,200,100,30),"开始游戏");
+	GUI.Button(Rect(230,240,100,30),"读取进度");
+	GUI.Button(Rect(230,280,100,30),"关于游戏");
+	GUI.Button(Rect(230,320,100,30),"退出游戏");
+}
+function DrawAnimation(tex:Object[],rect:Rect){
+	//绘制当前帧
+	GUI.DrawTexture(rect,tex[nowFram],ScaleMode.StretchToFill,true,0);
+	//计算限制帧时间
+	time+=Time.deltaTime;
+	//超过限制帧则切换图片
+	if(time>=1.0/fps){
+		//帧序列切换
+		nowFram++;
+		//限制帧清空
+		time=0;
+		//超过帧总数，从0开始
+		if(nowFram>=tex.Length){
+			nowFram=0;
+		}
+	}
+}
