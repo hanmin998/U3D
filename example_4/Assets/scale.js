b7990a7614b1ddfbe82e64881659cc4a5ebcf4e1
hanmin998 <853049349@qq.com>
2014-07-17 21:27:27 +0800

    scale
    scale

---
+++ example_4/Assets/scale.js
@@ -0,0 +1,18 @@
+﻿#pragma strict
+var obj:GameObject;
+var scaleX:float=1.0;
+var scaleY:float=1.0;
+var scaleZ:float=1.0;
+function Start () {
+	obj=GameObject.Find("Cube");
+}
+
+function OnGUI () {
+	GUILayout.Label("X轴缩放");
+	scaleX=GUILayout.HorizontalSlider(scaleX,1.0,2.0,GUILayout.Width(100));
+	GUILayout.Label("Y轴缩放");
+	scaleY=GUILayout.HorizontalSlider(scaleY,1.0,2.0,GUILayout.Width(100));
+	GUILayout.Label("Z轴缩放");
+	scaleZ=GUILayout.HorizontalSlider(scaleZ,1.0,2.0,GUILayout.Width(100));
+	obj.transform.localScale=Vector3(scaleX,scaleY,scaleZ);
+}
