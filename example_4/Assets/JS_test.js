70062fdb01b3f16848290320e2bf852a42341cc8
hanmin998 <853049349@qq.com>
2014-07-18 08:50:27 +0800

    C#与JS互相调用
    C#与JS互相调用

---
+++ example_4/Assets/JS_test.js
@@ -0,0 +1,12 @@
+﻿#pragma strict
+
+function OnGUI () {
+	if(GUI.Button(Rect(100,50,200,100),"JavaScript调用C#")){
+		var cs=this.GetComponent("CS_test");
+		cs.CallMe("我是来自JavaScript");
+	}
+}
+
+function CallMe (test:String) {
+	Debug.Log(test);
+}
