865813ef29a138788c1fab4258aef2c09b5784db
hanmin998 <853049349@qq.com>
2014-07-18 09:19:32 +0800

    Tools
    Tools

---
+++ example_5/Assets/Wait.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Wait : MonoBehaviour {
+
+	// Use this for initialization
+	IEnumerator Start () {
+		Debug.Log ("开始等待"+Time.time);
+		yield return new WaitForSeconds (2);
+		Debug.Log ("结束等待"+Time.time);
+	}
+}
