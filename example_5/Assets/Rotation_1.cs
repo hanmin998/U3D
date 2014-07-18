97f831b136a9997a31c4f19806d867c275ca0a34
hanmin998 <853049349@qq.com>
2014-07-18 10:13:38 +0800

    四元数
    精确计算模型旋转角度

---
+++ example_5/Assets/Rotation_1.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotation_1 : MonoBehaviour {
+	//是否开始插值旋转
+	bool isRotation=false;
+	// Use this for initialization
+	void OnGUI () {
+		if (GUILayout.Button ("旋转固定角度", GUILayout.Height (50))) {
+			gameObject.transform.rotation=Quaternion.Euler(0.0f,50.0f,0.0f);		
+
+		}
+		if (GUILayout.Button ("插值旋转固定角度", GUILayout.Height (50))) {
+			isRotation=true;		
+		}
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//开始插值旋转
+		if (isRotation) {
+			gameObject.transform.rotation=Quaternion.Slerp
+				(gameObject.transform.rotation,Quaternion.Euler(0.0f,50.0f,0.0f),
+				 Time.time*0.1f);		
+
+		}
+	
+	}
+}
