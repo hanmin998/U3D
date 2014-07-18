0ea5ba11fff0aff632a59cfc6f4ba23034b9d595
hanmin998 <853049349@qq.com>
2014-07-18 09:10:10 +0800

    工具类Time
    工具类Time

--- example_4/Assets/CS_test.cs
+++ example_4/Assets/CS_test.cs
@@ -1,12 +1,14 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+
 public class CS_test : MonoBehaviour {
+	public JS_test jsScript;
 
 	// Use this for initialization
 	void OnGUI () {
 		if (GUI.Button (new Rect (100, 170, 200, 100), "C#调用JavaScript")) {
-			JS_test jsScript=(JS_test)GetComponent("JS_test");
+			jsScript=(JS_test)GetComponent("JS_test");
 			jsScript.CallMe("我是来自C#");
 		}
 	}
---
+++ example_4/Assets/time.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class time : MonoBehaviour {
+
+	// Use this for initialization
+	void OnGUI () {
+		GUILayout.Label ("当前游戏时间："+Time.time);
+		GUILayout.Label ("上一帧所消耗的时间："+Time.deltaTime);
+		GUILayout.Label ("固定增量时间："+Time.fixedTime);
+		GUILayout.Label ("上一帧所消耗固定时间："+Time.fixedDeltaTime);
+	}
+	
+
+}
